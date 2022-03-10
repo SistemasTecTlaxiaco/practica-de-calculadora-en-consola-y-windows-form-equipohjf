616624dc1bfca35d676b4f0cbe05bf2e9cc12701
Federico <federicovictorianosierra@gmail.com>
2022-03-10 15:35:24 -0600

    Consola

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -9,48 +9,91 @@ namespace Consola
 {
     class Program
     {
+        private static double num1 = 0, num2 = 0, num0 = 0;
         static void Main(string[] args)
         {
-
-           
             bool salir = false;
-
             while (!salir)
             {
-
                 try
                 {
-
-                    Console.WriteLine("1. Introducir un numero");
-                    Console.WriteLine("2. Salir");
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("1. Sumar");
+                    Console.WriteLine("2. Restar");
+                    Console.WriteLine("3. Multiplicar");
+                    Console.WriteLine("4. Dividir");
+                    Console.WriteLine("5. Raiz Cuadrada");
+                    Console.WriteLine("6. Potencia");
+                    Console.WriteLine("7. Porcentaje");
+                    Console.WriteLine("8. Fraccion");
+                    Console.WriteLine("9. Salir");
+                    Console.WriteLine("------------------------------------------");
                     Console.WriteLine("Elige una de las opciones");
                     int opcion = Convert.ToInt32(Console.ReadLine());
 
                     switch (opcion)
                     {
                         case 1:
-                            Console.WriteLine("Introduzca un numero");
-                            double num = double.Parse(Console.ReadLine());
-                            Console.WriteLine("El resultado es: " + Class1.raizCuadrada(num));
+                            teclado1();
+                            Console.WriteLine("El resultado de la suma es: " + Class1.sumar(num1, num2));
                             break;
-
                         case 2:
+                            teclado1();
+                            Console.WriteLine("El resultado de la Resta es: " + Class1.restar(num1, num2));
+                            break;
+                        case 3:
+                            teclado1();
+                            Console.WriteLine("El resultado de la Multiplicacion es: " + Class1.multiplicar(num1, num2));
+                            break;
+                        case 4:
+                            teclado1();
+                            Console.WriteLine("El resultado de la Division es: " + Class1.division(num1, num2));
+                            break;
+                        case 5:
+                            teclado0();
+                            Console.WriteLine("El resultado de la Raiz cuadrada es: " + Class1.raizCuadrada(num0));
+                            break;
+                        case 6:
+                            teclado1();
+                            Console.WriteLine("El resultado de la Potencia es: " + Class1.potencia(num1, num2));
+                            break;
+                        case 7:
+                            teclado1();
+                            Console.WriteLine("El resultado del porcentaje es: " + Class1.porcentaje(num1, num2));
+                            break;
+                        case 8:
+                            teclado0();
+                            Console.WriteLine("El resultado de la fraccion es: " + Class1.fraccion(num0));
+                            break;
+                        case 9:
                             Console.WriteLine("Has elegido salir de la aplicación");
+                            Environment.Exit(1);
                             salir = true;
                             break;
                         default:
-                            Console.WriteLine("Elige una opcion entre 1 y 2");
+                            Console.WriteLine("Elige una opcion entre 1 y 9");
                             break;
                     }
 
                 }
                 catch (FormatException e)
                 {
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Error al ingresar!!");
                 }
             }
+        }
 
-            Console.ReadLine();
+        private static void teclado0()
+        {
+            Console.WriteLine("Introduzca un numero");
+            num0 = double.Parse(Console.ReadLine());
+        }
+        private static void teclado1()
+        {
+            Console.WriteLine("Introduzca el primer numero");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo numero");
+            num2 = double.Parse(Console.ReadLine());
         }
     }
 }
