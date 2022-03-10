f42351e9ff6068f833a5525e090938081668c0a2
Federico <minishxco@gmail.com>
2022-03-09 22:11:12 -0600

    Agregar archivos de proyecto.

---
+++ Consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LibreriaCalculadora;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un numero");
+            double Base = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro numero");
+            double Exponente = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es: " + Class1.potencia(Base, Exponente));
+        }
+    }
+}
---
+++ LibreriaCalculadora/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaCalculadora
+{
+    public class Class1
+    {
+        public static double sumar(double uno, double dos)
+        {
+            return uno + dos;
+        }
+        public static double multiplicar(double uno, double dos)
+        {
+            return uno * dos;
+        }
+
+        public static double mayor(double uno, double dos, double tres)
+        {
+            if (uno > dos && uno > tres)
+            {
+                return uno;
+            }
+            else
+            {
+                if (dos > uno && dos > tres)
+                {
+                    return dos;
+                }
+                else
+                {
+                    return tres;
+                }
+            }
+
+        }
+
+        public static double potencia(double Base, double exponente)
+        {
+            double resultado = Base;
+            for (int i = 1; i < exponente; i++)
+            {
+                resultado *= Base;
+            }
+            return resultado;
+        }
+
+        public static double division(double divisor, double dividendo)
+        {
+            return divisor / dividendo;
+        }
+
+        public static double raizCuadrada(double numero)
+        {
+            double resultado = 0;
+            for (double i = 1; true; i++)
+            {
+                if ((i * i) > numero)
+                {
+                    resultado = i - 1;
+                    break;
+                }
+            }
+            return resultado;
+        }
+    }
+}
