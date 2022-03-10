b3b240c16f1f52ee66b0dcb4d5aafd03c8cc2225
HEIDI LUCY BAUTISTA SANJUAN <heidi_lbs@tlaxiaco.tecnm.mx>
2022-03-10 14:15:31 -0600

    Mene consola

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using LibreriaCalculadora;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Consola
 {
@@ -7,9 +11,46 @@ namespace Consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduzca un numero");
-            double num = double.Parse(Console.ReadLine());
-            Console.WriteLine("El resultado es: " + Class1.raizCuadrada(num));
+
+           
+            bool salir = false;
+
+            while (!salir)
+            {
+
+                try
+                {
+
+                    Console.WriteLine("1. Introducir un numero");
+                    Console.WriteLine("2. Salir");
+                    Console.WriteLine("Elige una de las opciones");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Introduzca un numero");
+                            double num = double.Parse(Console.ReadLine());
+                            Console.WriteLine("El resultado es: " + Class1.raizCuadrada(num));
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Has elegido salir de la aplicación");
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Elige una opcion entre 1 y 2");
+                            break;
+                    }
+
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
